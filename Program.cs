f064423e044a617fc6760f0af2b99e00cc2972b9
Krzysztof Lachowski <s31425@pjwstk.edu.pl>
2026-03-17 20:34:57 +0100

    Dodanie nowej metody

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Git!");
+int result = Multiply(2, 3);
+Console.WriteLine(result);
+
+static int Multiply(int a, int b)
+{
+    return a * b;
+}
